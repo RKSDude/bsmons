b5e5d3d9dccfb36383be91409c753d2a8dc9adb6
RKSDude <anotherguywhoisbackup@gmail.com>
2023-03-21 15:27:38 +0800

    bvjvhj

--- Assets/Scenes/eiohjriqei.cs
+++ Assets/Scenes/eiohjriqei.cs
@@ -7,7 +7,7 @@ public class eiohjriqei : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //dawndlanwdlknawklda
+        //awdaw
     }
 
     // Update is called once per frame
